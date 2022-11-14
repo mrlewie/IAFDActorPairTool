fdbba85a4dfa8bf0f0915a51430a59d5360ce182
mrlewie <mrlewie@outlook.com>
2022-11-14 21:00:24 +0800

    Added

--- main.py
+++ main.py
@@ -144,3 +144,72 @@ if __name__ == '__main__':
             with open(os.path.join(out_actor_folder, output_file), 'w') as file:
                 json.dump(movie, file)
 
+
+
+
+# imports
+import os
+import json
+import numpy as np
+
+def show_all_movies_with_actor(data, actor, act):
+    stats = []
+
+    if actor is not None:
+        movies = [d for d in data if actor == d.get('pair_actor')]
+    else:
+        print('No actor of that name.')
+        return
+
+    if act is not None:
+        movies = [m for m in movies if act in m.get('acts')]
+
+    act = 'Anything' if act is None else act
+
+    if len(movies) > 0:
+        for movie in movies:
+            print('{} with {} in {} ({})'.format(act, ', '.join([m for m in movie['cast']]), movie['name'], movie['url']))
+
+
+def show_num_scenes_per_actor(data, act):
+    stats = []
+
+    if act is not None:
+        movies = [d for d in data if act in d.get('acts')]
+    else:
+        movies = data
+
+    act = 'Anything' if act is None else act
+
+    if len(movies) > 0:
+        appeared_with = [m.get('pair_actor') for m in movies]
+        uniques, counts = np.unique(appeared_with, return_counts=True)
+        stats = sorted(zip(counts, uniques), reverse=True)
+
+        for stat in stats:
+            print('{} with {}: {} times.'.format(act, stat[1], stat[0]))
+
+
+
+if __name__ == '__stats__':
+
+    # set actor page url (find on iafd)
+    actor_folder = r'C:\Users\Lewis\Desktop\pairings\Jenner'
+
+    i = 0
+
+    # load existing data if exists
+    data = []
+    if os.path.exists(actor_folder):
+        files = [os.path.join(actor_folder, file) for file in os.listdir(actor_folder)]
+
+        for file in files:
+            with open(file, 'r') as fp:
+                data.append(json.load(fp))
+
+
+    # count how many times current actor worked with another
+    show_num_scenes_per_actor(data, 'DP')
+
+    # show movies with specific actor and act
+    show_all_movies_with_actor(data, 'Rick Masters', 'DP')
